7f812bff7996316cf5e4efdc3d2319b63ed769e5
Don Herod <don.herod@gmail.com>
2015-08-31 23:29:02 -0400

    Remove template code and add app routing through menu and links

---
+++ DesignToolbox/Controllers/ToolController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DesignToolbox.Controllers
+{
+    public class ToolController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Basics()
+        {
+            ViewBag.Title = "OO Basics";
+
+            return View();
+        }
+
+        public ActionResult Principles()
+        {
+            ViewBag.Title = "OO Principles";
+
+            return View();
+        }
+
+        public ActionResult Patterns()
+        {
+            ViewBag.Title = "OO Patterns";
+
+            return View();
+        }
+    }
+}
